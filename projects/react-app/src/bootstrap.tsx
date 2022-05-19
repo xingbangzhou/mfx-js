7fc2dabb1e3dabebb8d0b590a3733c71caf7a0a5
zhouxingbang <zhouxingbang@yy.com>
2022-05-19 23:13:20 +0800

    feat: 调整样式目录

--- projects/react-app/src/bootstrap.tsx
+++ projects/react-app/src/bootstrap.tsx
@@ -1,5 +1,5 @@
+import './assets/styles/base.scss'
 import ReactDOM from 'react-dom'
 import App from './pages/App'
-import './styles/index.scss'
 
 ReactDOM.render(<App></App>, document.getElementById('root'), () => {})
